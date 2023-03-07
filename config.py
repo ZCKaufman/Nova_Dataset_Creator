f4955ee47fd66b3ac7e4a003a86722ff9dfbf787
Zachary Kaufman <zckaufman@Zacharys-MacBook-Pro-3.local>
2023-03-06 20:53:15 -0500

    Added config.py checker

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
-fileName = ""
+fileName = "LL_KM_RPS_gaze_12_2.csv"
 videoName = "KM_rps_1_RGB.avi"
 outputPath = "/Users/zckaufman/Documents/Work/RHouse/Haru_Dataset/" # INCLUDE output folder. Ex: "/Users/zckaufman/Documents/Work/RHouse/Haru_Dataset/"
-labels = ["1", "2", "3"] # Must match labels from CSV
+labels = ["Activity", "SDK Monitor", "Robot", "Look at Leigh"] # Must match labels from CSV
 numLabels = len(labels)
 mode = "gaze" # Options: gaze, thermal
--- label.py
+++ label.py
@@ -3,8 +3,39 @@
 import config as cfg
 import cv2
 import math
+import os
+import sys
 
 def configCheck():
+    print("--- Checking and running configuration from config.py ---")
+
+    # Check output path
+    #if(cfg.outputPath and cfg.outputPath != ""):
+    #    print("CONFIG: Output path exists and is not empty")
+    #else:
+    #    return False
+
+    if os.path.exists(cfg.outputPath) and not os.path.isfile(cfg.outputPath):
+        print("CONFIG: Output path exists")
+        if os.listdir(cfg.outputPath):
+            print("CONFIG: Output path found but is not empty. Output path must be empty. Terminating program.")
+            sys.exit()
+        else:
+            print("CONFIG: Output path is empty")
+    else:
+        print("CONFIG: Output path either does not exist or is a file. Output path must exist and be a directory.\nTerminating program.")
+        sys.exit()
+
+    # Check and create labels
+    if(cfg.numLabels <= 0):
+        return False
+    else:
+        print("CONFIG: Labels found")
+    for i in cfg.labels:
+        path = os.path.join(cfg.outputPath, i)
+        os.mkdir(path)
+        print("CONFIG: Created label -", i)
+    print("CONFIG: All found labels created")
 
     return True
 
@@ -18,22 +49,21 @@ def label():
         success, frame = capture.read()
  
         if success:
-            #cv2.imwrite(cfg.outputPath + "{}:{:.2f}.jpg".format(math.floor(timestamp), (timestamp % 1) * 100), frame)
             cv2.imwrite(cfg.outputPath + "{:02d}_{:02d}.jpg".format(math.floor(timestamp), math.floor(timestamp % 1 * 100)), frame)
         else:
             break
         
         timestamp = timestamp + 0.0667
-        if(round(timestamp, 2) % 1 == 0): 
+        if(math.floor(timestamp) == i): 
+            i = i+1
             print("{:.1f} Seconds Converted".format(timestamp))
-        i = i+1
  
     capture.release()
     return
 
 if __name__ == '__main__':
-    if configCheck():
-        label()
-        print("Labeling complete. Output images with labels can be found at: ", cfg.outputPath)
-    else:
-        print("Config file not complete. Please give all variables a value in config.py.")
+    #if configCheck():
+    label()
+     #   print("Labeling complete. Output images with labels can be found at: ", cfg.outputPath)
+   # else:
+    #    print("Config file not complete. Please give all variables a value in config.py.")
